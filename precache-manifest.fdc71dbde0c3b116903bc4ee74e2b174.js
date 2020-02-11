e436c01d04a5d3e8732d50b5e336fc9f7c529d5b
Krzysztof <krzysztofs012@outlook.com>
2020-02-11 20:50:14 +0100

    Updates

---
+++ precache-manifest.fdc71dbde0c3b116903bc4ee74e2b174.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebdbcf505eb790532f8cea7edd7a7acd",
+    "url": "/basic-react-app/index.html"
+  },
+  {
+    "revision": "b305ba75bf12f44beebb",
+    "url": "/basic-react-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d846dbda0ba8fb79b47c",
+    "url": "/basic-react-app/static/js/2.587876a3.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/basic-react-app/static/js/2.587876a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b305ba75bf12f44beebb",
+    "url": "/basic-react-app/static/js/main.df5c8a23.chunk.js"
+  },
+  {
+    "revision": "1eb79dd8ff480e3d9630",
+    "url": "/basic-react-app/static/js/runtime-main.f9f4699c.js"
+  }
+]);
